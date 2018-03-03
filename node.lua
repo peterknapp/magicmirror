d266533f18835e5aef6fcf47423bc9bc51640f01
Peter Knapp <peterknapp.developer@gmail.com>
2018-03-03 16:43:42 +0100

    6

--- node.lua
+++ node.lua
@@ -1,21 +1,20 @@
--- gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
-
--- local font = resource.load_font "default-font.ttf"
--- local text = ""
-
--- util.json_watch("config.json", function(config)
---     -- called each time the configuration is changed. 'config'
---     -- contains new current configuration values set by the user.
-
---     text = config.text -- assign configuration 'text' value to 'text' variable
--- end)
-
--- function node.render()
---     gl.clear(0,0,0,1)
---     font:write(250, 300, text, 64, 1,1,1,1) -- use the CONFIG value
--- end
-
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
+
+local font = resource.load_font "default-font.ttf"
+local text = ""
+
+util.json_watch("config.json", function(config)
+    -- called each time the configuration is changed. 'config'
+    -- contains new current configuration values set by the user.
+
+    text = config.text -- assign configuration 'text' value to 'text' variable
+end)
+
+function node.render()
+    gl.clear(0,0,0,1)
+    font:write(250, 300, text, 64, 1,1,1,1) -- use the CONFIG value
+end
+
 node.alias "looper"
 
 -- This version uses low level video playback 
