154cc709ad9a0dfdd4ce113c2670097ebf1b1890
Peter Knapp <peterknapp.developer@gmail.com>
2018-03-03 16:15:04 +0100

    first package

--- node.lua
+++ node.lua
@@ -1,7 +1,16 @@
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 
-local font = resource.load_font "default-font.ttf"
+local font = resource.load_font "font.ttf"
+local text = "Hello World"
+
+util.json_watch("config.json", function(config)
+    -- called each time the configuration is changed. 'config'
+    -- contains new current configuration values set by the user.
+
+    text = config.text -- assign configuration 'text' value to 'text' variable
+end)
 
 function node.render()
-    font:write(250, 300, "Hello world", 64, 1,1,1,1)
+    gl.clear(0,0,0,1)
+    font:write(250, 300, text, 64, 1,1,1,1) -- use the CONFIG value
 end
